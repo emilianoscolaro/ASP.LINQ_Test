09006dcf968ba003dc6d267f1a95b60dbdbdce3b
EMI2 SCOLARO <escolaroo@gmail.com>
2020-08-19 14:17:35 -0300

    Agregar archivos de proyecto.

---
+++ ASP.LINQ_Test/Controllers/AreaController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.LINQ_Test.Models;
+
+namespace ASP.LINQ_Test.Controllers
+{
+    public class AreaController : Controller
+    {
+        AreaModel model = new AreaModel();
+
+        // GET: Area
+        public ActionResult Index()
+        {
+            return View(model.ListaAreas());
+        }
+
+        public ActionResult Detalles(int id)
+        {
+            var area = model.BuscarArea(id);
+            return View(area);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(area a)
+        {
+            try
+            {
+                if (model.InsertarArea(a).Equals("OK"))
+                {
+                    return RedirectToAction("Index");
+                }
+                else { return RedirectToAction("Create"); }
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            var area = model.BuscarArea(id);
+            return View(area);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id,area a)
+        {
+            try
+            {
+                area area = new area();
+                area.are_cod = id;
+                area.are_des = a.are_des;
+
+                if (model.ActualizarArea(area).Equals("OK"))
+                {
+                    return RedirectToAction("Index");
+                }
+                else { return RedirectToAction("Edit"); }
+
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var area = model.BuscarArea(id);
+            return View(area);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id,FormCollection collection)
+        {
+            try
+            {
+                if (model.EliminarArea(id).Equals("OK"))
+                {
+                    return RedirectToAction("Index");
+                }
+                else {return RedirectToAction("Delete"); }
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+
+
+        }
+
+    }
+}
---
+++ ASP.LINQ_Test/Models/AreaModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace ASP.LINQ_Test.Models
+{
+    public class AreaModel
+    {
+        DataClasses1DataContext context = new DataClasses1DataContext();
+
+        public List<area> ListaAreas()
+        {
+            List<area> Lista = new List<area>();
+            var consulta = context.PrListarArea();
+
+            foreach (var area in consulta)
+            {
+                area a = new area();
+
+                a.are_cod = area.are_cod;
+                a.are_des = area.are_des;
+
+                Lista.Add(a);
+
+            }
+
+            return Lista;
+
+        }
+
+
+        public area BuscarArea(int codigo)
+        {
+            area a = new area();
+
+            try
+            {
+                var consulta = context.PrBuscarArea(codigo);
+                foreach (var area in consulta)
+                {
+                    a.are_cod = area.are_cod;
+                    a.are_des = area.are_des;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return a;
+        }
+
+        public string InsertarArea(area a)
+        {
+            string resultado = string.Empty;
+
+            try
+            {
+                context.PrInsertarArea(a.are_des);
+                resultado = "OK";
+            }
+            catch (Exception ex)
+            {
+                resultado = ex.Message;
+            }
+            return resultado;
+        }
+
+        public string ActualizarArea(area a)
+        {
+            string resultado = string.Empty;
+
+            try
+            {
+                context.PrActualizarArea(a.are_cod,a.are_des);
+                resultado = "OK";
+            }
+            catch (Exception ex)
+            {
+                resultado = ex.Message;
+            }
+            return resultado;
+        }
+
+
+        public string EliminarArea(int codigo)
+        {
+            string resultado = string.Empty;
+
+            try
+            {
+                context.PrEliminar(codigo);
+                resultado = "OK";
+            }
+            catch (Exception ex)
+            {
+                resultado = ex.Message;
+            }
+
+            return resultado;
+        }
+
+    }
+}
